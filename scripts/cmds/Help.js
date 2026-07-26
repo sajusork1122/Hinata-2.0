59e8245435d71a2c734e0b3c5fe2ca0fec5597d4
Ahmed shishir <sajusorkar112233@gmail.com>
2026-07-26 18:50:12 +0600

    Update author name in Help.js

--- scripts/cmds/Help.js
+++ scripts/cmds/Help.js
@@ -5,7 +5,7 @@ module.exports = {
         config: {
                 name: "help",
                 version: "1.7",
-                author: "MahMUD",
+                author: "𝙎𝙃𝙄𝙎𝙃𝙄𝙍 ",
                 countDown: 5,
                 role: 0,
                 shortDescription: {
